ed01e1b4fe650e4ea952394d5c0f6e1112583080
Hitesh sharma <hiteshsharma200826@gmail.com>
2025-11-11 20:50:56 +0530

    2026day prediction
    This program predict the day of everyv month of 2026

---
+++ 2026clander.c
@@ -0,0 +1,254 @@
+//program for predict the day name according to 2026 clander all month's
+#include<stdio.h>
+int main()
+{ 
+printf("Enter any date::");
+int a,s;
+scanf("%d",&s);
+a=s%7;
+printf("Enter a month::");
+int month;
+scanf("%d",&month);
+if(month==1)
+{
+if(a==4)
+printf("\nSunday");
+else if(a==5) 
+printf("\nmonday");
+else if(a==6) 
+printf("\ntuesday");
+else if(a==7)  
+printf("\nwednesday");
+else if(a==1)  
+printf("\nthursday");
+else if(a==2)  
+printf("\nfriday");
+else if(a==3)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==2)
+{
+if(a==1)
+printf("\nSunday");
+else if(a==2) 
+printf("\nmonday");
+else if(a==3) 
+printf("\ntuesday");
+else if(a==4)  
+printf("\nwednesday");
+else if(a==5)  
+printf("\nthursday");
+else if(a==6)  
+printf("\nfriday");
+else if(a==7)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==3)
+{
+if(a==1)
+printf("\nSunday");
+else if(a==2) 
+printf("\nmonday");
+else if(a==3) 
+printf("\ntuesday");
+else if(a==4)  
+printf("\nwednesday");
+else if(a==5)  
+printf("\nthursday");
+else if(a==6)  
+printf("\nfriday");
+else if(a==7)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==4)
+{
+if(a==4)
+printf("\nSunday");
+else if(a==5) 
+printf("\nmonday");
+else if(a==6) 
+printf("\ntuesday");
+else if(a==7)  
+printf("\nwednesday");
+else if(a==1)  
+printf("\nthursday");
+else if(a==2)  
+printf("\nfriday");
+else if(a==3)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==5)
+{
+if(a==3)
+printf("\nSunday");
+else if(a==4) 
+printf("\nmonday");
+else if(a==5) 
+printf("\ntuesday");
+else if(a==6)  
+printf("\nwednesday");
+else if(a==7)  
+printf("\nthursday");
+else if(a==1)  
+printf("\nfriday");
+else if(a==2)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==6)
+{
+if(a==7)
+printf("\nSunday");
+else if(a==1) 
+printf("\nmonday");
+else if(a==2) 
+printf("\ntuesday");
+else if(a==3)  
+printf("\nwednesday");
+else if(a==4)  
+printf("\nthursday");
+else if(a==5)  
+printf("\nfriday");
+else if(a==6)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==7)
+{
+if(a==4)
+printf("\nSunday");
+else if(a==5) 
+printf("\nmonday");
+else if(a==6) 
+printf("\ntuesday");
+else if(a==7)  
+printf("\nwednesday");
+else if(a==1)  
+printf("\nthursday");
+else if(a==2)  
+printf("\nfriday");
+else if(a==3)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==8)
+{
+if(a==2)
+printf("\nSunday");
+else if(a==3) 
+printf("\nmonday");
+else if(a==4) 
+printf("\ntuesday");
+else if(a==5)  
+printf("\nwednesday");
+else if(a==6)  
+printf("\nthursday");
+else if(a==7)  
+printf("\nfriday");
+else if(a==1)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==9)
+{
+if(a==6)
+printf("\nSunday");
+else if(a==7) 
+printf("\nmonday");
+else if(a==1) 
+printf("\ntuesday");
+else if(a==2)  
+printf("\nwednesday");
+else if(a==3)  
+printf("\nthursday");
+else if(a==4)  
+printf("\nfriday");
+else if(a==5)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==10)
+{ 
+if(a==4)
+printf("\nSunday");
+else if(a==5) 
+printf("\nmonday");
+else if(a==6) 
+printf("\ntuesday");
+else if(a==7)  
+printf("\nwednesday");
+else if(a==1)  
+printf("\nthursday");
+else if(a==2)  
+printf("\nfriday");
+else if(a==3)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==11)
+{
+if(a==1)
+printf("\nSunday");
+else if(a==2) 
+printf("\nmonday");
+else if(a==3) 
+printf("\ntuesday");
+else if(a==4)  
+printf("\nwednesday");
+else if(a==5)  
+printf("\nthursday");
+else if(a==6)  
+printf("\nfriday");
+else if(a==7)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+if(month==12)
+{
+if(a==6)
+printf("\nSunday");
+else if(a==7) 
+printf("\nmonday");
+else if(a==1) 
+printf("\ntuesday");
+else if(a==2)  
+printf("\nwednesday");
+else if(a==3)  
+printf("\nthursday");
+else if(a==4)  
+printf("\nfriday");
+else if(a==5)  
+printf("\nsaturday");
+else 
+printf("\n!!PLEASE ENTER THE RIGHT DATE");
+exit(0);
+}
+printf("Year :  2026");
+return 0;
+}
