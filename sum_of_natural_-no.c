c44eccca1716bbf263f9366dfa9af512082f4ae2
Hitesh sharma <hiteshsharma200826@gmail.com>
2025-11-14 21:16:35 +0530

    Sumofnaturalno
    In this program i write a code to find the sum of natural numbers. From user i take input of range of natural numbers.Theni use for lop to count the number of terms in input range.Then use the formula of ap to find out the sum.And then print sum..

---
+++ sum_of_natural_-no.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,s;
+printf("\nEnter the range of naturl numbers::");
+scanf("%d%d",&a,&s);
+int i=a;
+int n;
+for(;i<=s;i++)
+{
+n=i;
+}
+printf("\nnumber%d",n);
+int sum=n/2 * (a+s);
+printf("\nSum of %d to %d natural number is___%d",a,s,                                                                                                                         sum);
+return 0;
+}
+  
